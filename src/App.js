9bc38a2ac4483f07488fc2dc3807b3b5914c8e60
EvanBinga <106447704+EvanBinga@users.noreply.github.com>
2022-11-28 19:26:21 +0300

    2
    2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import ToDo from './ToDo';
+import ToDoForm from './ToDoForm';
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTask =() => {
+
+  }
+
+  const removeTask =() => {
+    
+  }
+
+  const handleTask =() => {
+    
+  }
+
+
+
   return (
     <div className="App">
-    
+      <header>
+        <h1>Cписок задач: {todos.length}</h1>
+      </header>
+      <ToDoForm />
+      {todos.map(() => {
+        return (
+          <ToDo
+          key={todos.id}
+          />
+        )
+      })}
     </div>
   );
 }
